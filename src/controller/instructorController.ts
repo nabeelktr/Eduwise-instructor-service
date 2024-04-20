5c96635738c116b99291bef741b2e8c59c604817
nabeelktr <nabee446@gmail.com>
2024-04-20 12:52:03 +0530

    instructor registration handler added

--- src/controller/instructorController.ts
+++ src/controller/instructorController.ts
@@ -1,12 +1,11 @@
 import { IInstructorService } from "../interfaces/iInstructor";
-import { Instructor } from "../model/instructor.entities";
+
 
 export class InstructorController {
 
     constructor(private service : IInstructorService) {}
 
     registerInstructor = async (data: any) => {
-        console.log(data);
         try{
             const response = await this.service.userRegister(data)
             return response;
